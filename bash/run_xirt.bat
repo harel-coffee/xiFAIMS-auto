fea7784e0915731f61beadb191a4412a9b26b9cf
Sven <sven.giese88@gmail.com>
2020-10-16 14:18:12 +0200

    adapt naming

--- bash/run_xirt.bat
+++ bash/run_xirt.bat
@@ -1,11 +1,11 @@
 #normal reg
-#python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_reg.yaml ../parameters/xirt_learning.yaml
+#python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_reg_aux0.yaml ../parameters/xirt_learning.yaml
 #python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_reg_aux1.yaml ../parameters/xirt_learning.yaml
 #python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_reg_aux2.yaml ../parameters/xirt_learning.yaml
 python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_reg_aux3.yaml ../parameters/xirt_learning.yaml
 												
 #normal ordinal                                             
-#python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_ordinal.yaml ../parameters/xirt_learning.yaml
+#python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_ordinal_aux0.yaml ../parameters/xirt_learning.yaml
 #python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_ordinal_aux1.yaml ../parameters/xirt_learning.yaml
 #python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_ordinal_aux2.yaml ../parameters/xirt_learning.yaml
 python ../xirt_analysis.py ../data/4PM_DSS_LS_nonunique1pCSM.csv ../parameters/xirt_faims_ordinal_aux3.yaml ../parameters/xirt_learning.yaml
