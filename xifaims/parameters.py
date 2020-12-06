b810062a44da6f1b7a887aadc20a0e760f6dba9d
Sven <sven.giese88@gmail.com>
2020-12-06 08:45:22 +0100

    adapt paraemters to avoid overfitting in huge parameters

--- xifaims/parameters.py
+++ xifaims/parameters.py
@@ -15,17 +15,19 @@ xgb_large = {'n_estimators': [50, 100, 150],
              'gamma': [0.0, 0.5, 1],
              'subsample': [0.8, 1.0],
              'colsample_bytree': [0.8, 1.0],
-             'max_depth': [3, 4, 5],
+             'max_depth': [3, 6, 9],
              'learning_rate': [0.01, 0.1, 0.3],
              'nthread': [1], 'seed': [42]}
 
 xgb_huge = {'n_estimators': [30, 50, 100],
              'max_depth': [3, 5, 7, 9],
-             'min_child_weight': [0.001, 0.1, 1],
-             'learning_rate': [0.001, 0.01, 0.1, 0.3],
-             'gamma': [0.0, 0.1, 0.2, 0.3],
+             'min_child_weight': [0.1, 1, 10],
+             'learning_rate': [0.01, 0.1, 0.3],
+             'gamma': [0.0, 0.1],
              'reg_alpha': [1e-5, 1e-2, 0.1],
              'reg_lambda': [1e-5, 1e-2, 0.1],
+            'subsample': [0.5, 0.8, 1.0],
+            'colsample_bytree': [0.5, 0.8, 1.0],
              'nthread': [1], 'seed': [42]}
 
 xgb_params = {"tiny": xgb_tiny, "small": xgb_small, "large": xgb_large, "huge": xgb_huge}
